5573baae979dc66f75383be12329cf0847207c70
Mikołaj Petecki <mikolajpe2@gmail.com>
2024-12-11 18:49:22 +0100

    day11 part 2 with cache

--- day11/day11.py
+++ day11/day11.py
@@ -1,4 +1,4 @@
-stones_counter = 0
+from functools import lru_cache
 
 def part_one(stones, iterations=25):
     i = 0
@@ -14,49 +14,45 @@ def part_one(stones, iterations=25):
                 new_stones.append(stone * 2024)
         stones = new_stones
 
-        print(f"koniec iteracji {i + 1}, dlugosc listy: {len(stones)}")
-
         i += 1
 
     return len(stones)
 
 
-def part_two(stones, iterations=30):
+def part_two(stones, iterations=25):
     return part_one(stones, iterations)
 
 
-def part_two_recurrence(stones, iterations=30):
-    global stones_counter
+def part_two_recurrence(stones, iterations=25):
+    stones_counter = 0
 
     for stone in stones:
-        stones_counter += 1
-        part_two_recurrence(stone, iterations - 1)
+        stones_counter += blink(stone, iterations - 1)
 
     return stones_counter
 
 
-def part_two_recurrence(stone, iteration):
-    global stones_counter
+@lru_cache(maxsize=None)
+def blink(stone, iteration):
 
     if iteration == -1:
-        return
+        return 1
     
     if stone == 0:
-        part_two_recurrence(1, iteration - 1)
+        return blink(1, iteration - 1)
 
     elif len(str(stone)) % 2 == 0:
-        stones_counter += 1
-        part_two_recurrence(int(str(stone)[:len(str(stone)) // 2]), iteration - 1)
-        part_two_recurrence(int(str(stone)[len(str(stone)) // 2:]), iteration - 1)
+        return blink(int(str(stone)[:len(str(stone)) // 2]), iteration - 1) + blink(int(str(stone)[len(str(stone)) // 2:]), iteration - 1)
 
     else:
-        part_two_recurrence(stone * 2024, iteration - 1)
+        return blink(stone * 2024, iteration - 1)
 
 
 if __name__ == '__main__':
 
-    with open('example.txt') as f:
+    with open('input.txt') as f:
         stones = list(map(int, f.read().strip().split(" ")))
 
     print(part_one(stones))
-    print(part_two(stones))
+    # print(part_two(stones))
+    print(part_two_recurrence(stones, iterations=75))
